a62042d84bac42feaadbcf2649a39cc99bf6c4a0
Romain C <romain.combet@sanofi.com>
2018-12-07 12:17:18 +0100

    pmxplore update integration

--- inst/popkinr/dataset_exploration/ui.R
+++ inst/popkinr/dataset_exploration/ui.R
@@ -16,33 +16,43 @@ library(rhandsontable)
 library(shinycssloaders)
 library(shinyjs)
 library(xlsx)
+library(PKNCA)
+library(shinyWidgets)
+library(zoo)
+
+###
+library(xml2)
+
 
 source("chooser.R")
   useShinyjs()  # Include shinyjs
+
+
 shinyUI(
 dashboardPage(
-  
+
   skin = "purple",
   title = "PMxplore Dataset Exploration",
 
   dashboardHeader(title = "PMxplore Montpellier", titleWidth = 400),
-  
+
   dashboardSidebar(
     sidebarMenu(
-      
+
       menuItem("Home", tabName = "dashboard", icon = icon("home"), selected = TRUE),
-      
+
       menuItem("Dataset View", tabName = "data_view", icon = icon("tv") ),
-      
+
       menuItem("Dependent Variable",tabName = "DV_tab", icon = icon("gamepad")),
-     
+
       menuItem("Covariates", icon = icon("pie-chart"),
                menuSubItem("Continuous covariates", tabName = "continuous_tab", icon = icon("fighter-jet")),
                menuSubItem("Categorical covariates", tabName = "categorical_tab", icon = icon("database"))
                ),
       menuItem("Manage variables type",tabName="manage_tab", icon = icon("cogs")),
       menuItem("Enrich the dataset",tabName = "enrich_tab", icon = icon("plus")),
-      useShinyjs(),  # Include shinyjs      
+      menuItem("NCA",tabName = "NCA_tab", icon = icon("signal")),
+      useShinyjs(),  # Include shinyjs
       br(),
       hr(),
       div(align = "center",
@@ -54,8 +64,8 @@ dashboardPage(
     )
   ),
   dashboardBody(
-    
-    
+
+
    # shiny::includeCSS('styles.css'),
        tabItems(
          tabItem(tabName = "dashboard",
@@ -66,9 +76,10 @@ dashboardPage(
                    #Selector for file upload
                    fileInput('datafile', 'Choose .CSV / .DAT dataset',
                              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+                   actionLink("click_browse", "Browse..."),
                    actionLink("Demo","Demo dataset",icon("edit"))
                  ),
-                 
+
                  box(width=12,
                     valueBoxOutput(width = 5,"check_box")),
                  box(title="Overall information:",width=12,
@@ -76,9 +87,9 @@ dashboardPage(
                     valueBoxOutput(width = 3, "observations_box"),
                     valueBoxOutput(width = 3, "compartments_box"),
                     valueBoxOutput(width = 3, "doses_box"))
-                    
-                    
-                 
+
+
+
                   )
                 ),
          tabItem(tabName = "data_view",
@@ -93,9 +104,9 @@ dashboardPage(
                    column(width=3,uiOutput("Filter_dose")),
                    column(width=3,uiOutput("Level_dose"))
                     # column(width=3,uiOutput("DV_choice"))
-                                  
+
                    )),
-                  
+
                  fluidRow(width=12,
                           column(width=2,uiOutput("TimeVar")),
                           column(width=2,uiOutput("Split_dose"),
@@ -106,18 +117,18 @@ dashboardPage(
                           column(width=2,a(id = "toggle_others_options", "show / hide others options", href = "#")),
                           div(id="others_options",column(width=2,uiOutput("Other_filter"),
                                         conditionalPanel("input.Other_filter!=='No filter'",uiOutput("filt_Value"))),
-                          
+
                           column(width=2,uiOutput("Split_other")))
-                 
-                 
-                
+
+
+
                  ,
                  fluidRow(box(
                    width = 12, title = "DV versus TIME",
-                   
+
                     withSpinner(plotlyOutput('DV_graph',height = "100%"))
                     # withSpinner(plotOutput('DV_graph'))
-                               # , type = getOption("spinner.type", default = 4), 
+                               # , type = getOption("spinner.type", default = 4),
                                #                                          color = getOption("spinner.color", default = "#67429f"))
                  )
                  ),
@@ -138,7 +149,7 @@ dashboardPage(
                    width = 12, title = "Covariates Selection",
                    uiOutput("ContinuousVar")
                            )),
-                 
+
                  fluidRow(box(
                    width = 12, title = "Plot Type",
                    column(radioButtons("Plot_type","Choose plot type", choices=c("Boxplot" = 0, "Violin" = 1,"Histogram" = 2, "Density" = 3),inline=TRUE, selected=0),width = 6),
@@ -175,19 +186,19 @@ dashboardPage(
                    width = 6, title = "Splitting options",
                    uiOutput("SplitCatVar"),
                    uiOutput("Split2CatVar")
-                   
+
                          )
                    ),
-                 
-                 
+
+
                  fluidRow(box(
                    width = 12, title = "Plot",
                    plotlyOutput("CatPlot",height = "100%")
                  )),
-                 
+
                  fluidRow(box(
                    width = 12, title = "Descriptive Statistics",
-                   
+
                    tableOutput('stat_cat_table'),
                    downloadButton("download_stats_cat", label="export")
                    #actionButton('Help',label='Help')
@@ -195,7 +206,7 @@ dashboardPage(
                           )
                  ),
          tabItem(tabName = "manage_tab",
-                 
+
                   fluidRow(box(
                       width = 4, title = h1("Computed Class of Variable"),
                       br(),h4(strong("Continuous   /    Categorical")),
@@ -216,10 +227,10 @@ dashboardPage(
                    box(width=4, title = strong("INFO 1: Number of modalities by variable"),dataTableOutput("modalite_tab")),
                    box(width=8, title = strong("INFO 2: List of variables changing over time"),
                        verbatimTextOutput("COT_varlist"),
-                       actionButton("COT_details",label="Modalities by ID",icon =icon("list-alt", lib = "glyphicon"), 
-                                    style = "color: white; 
-                     background-color: #56739A; 
-                     position: relative; 
+                       actionButton("COT_details",label="Modalities by ID",icon =icon("list-alt", lib = "glyphicon"),
+                                    style = "color: white;
+                     background-color: #56739A;
+                     position: relative;
                      left: 3%;
                      height: 35px;
                      width: 130px;
@@ -227,12 +238,12 @@ dashboardPage(
                      text-indent: -2px;
                      border-radius: 6px;
                      border-width: 2px"),
-                       
+
                        conditionalPanel("input$switchinput==T",dataTableOutput("count_mbi")))
-                       
-                   
+
+
                    )
-         
+
                 )
           ,
            tabItem(tabName = "enrich_tab",
@@ -240,9 +251,9 @@ dashboardPage(
                      box(width=4,title=h3("Sequences"),
                          rHandsontableOutput("hot"),br(),
                          actionButton("Addrow","Add a row",
-                                      style = "color: black; 
-                                                 background-color: #CCD1D1; 
-                                                 position: relative; 
+                                      style = "color: black;
+                                                 background-color: #CCD1D1;
+                                                 position: relative;
                                                  left: 3%;
                                                  height: 35px;
                                                  width: 150px;
@@ -251,9 +262,9 @@ dashboardPage(
                                                  border-radius: 6px;
                                                  border-width: 3px"),
                          actionButton("Delrow","Delete a row",
-                                        style = "color: black; 
-                                                 background-color: #CCD1D1; 
-                                                 position: relative; 
+                                        style = "color: black;
+                                                 background-color: #CCD1D1;
+                                                 position: relative;
                                                  left: 3%;
                                                  height: 35px;
                                                  width: 150px;
@@ -266,9 +277,9 @@ dashboardPage(
                          #conditionalPanel("input.ColOption == TRUE",uiOutput("fillcol")),
                          br(),br(),br(),
                          actionButton("Run",label="  RUN",icon =icon("play", lib = "glyphicon"),
-                                      style = "color: black; 
-                                                 background-color: #4682B4; 
-                                                 position: relative; 
+                                      style = "color: black;
+                                                 background-color: #4682B4;
+                                                 position: relative;
                                                  left: 3%;
                                                  height: 35px;
                                                  width: 150px;
@@ -277,9 +288,9 @@ dashboardPage(
                                                  border-radius: 6px;
                                                  border-width: 3px"),br(),
                          downloadButton("download1", label="Download", class = "butt1",
-                                        style = "color: black; 
-                                                 background-color: #CCD1D1; 
-                                                 position: relative; 
+                                        style = "color: black;
+                                                 background-color: #CCD1D1;
+                                                 position: relative;
                                                  left: 3%;
                                                  height: 35px;
                                                  width: 150px;
@@ -287,10 +298,129 @@ dashboardPage(
                                                  text-indent: -2px;
                                                  border-radius: 6px;
                                                  border-width: 3px")
-                         
+
                          ),
                      box(width=8,title=h3("New dataset preview:"),DT::dataTableOutput('richdata'))
-                                     ))
+                                     )),
+         tabItem(tabName = "NCA_tab",
+                 fluidRow(
+                   box(width=4,
+                 uiOutput("NCA_cmt_dose_choice"),
+                 uiOutput("NCA_cmt_conc_choice"),
+                 uiOutput("NCA_TimeVar"),
+                 radioButtons("dose.choice","Dose choice : ",choices=c("First","Last","Other"),inline = T),
+                 numericInput("NCA_dosenumber","Choose dose number:",value=2,min=2,step=1),
+                 uiOutput("NCA_DVchoice"),
+                 # shinyjs::disabled(selectInput("interest.var","Variable of interest : ", choices=c("DV","DV2","DV3"),multiple=F)),
+                 materialSwitch("MDV.delete", strong("Remove MDV=1"), status = "success", value=F),
+                 # radioButtons("NCA_TimeUnit","Unit of time variable in the dataset",choices=c("Minutes","Hours","Days"), inline = T),
+                 # materialSwitch("Auto_interval", "Automatic Intervals", status = "info", value=T),
+
+                 strong("Time intervals for calculation"),br(),br(),
+
+
+                 rHandsontableOutput("NCA_hot"),br(),
+                 actionButton("NCA_Addrow","Add an interval",
+                              style = "color: black;
+                                                 background-color: #CCD1D1;
+                                                 position: relative;
+                                                 left: 3%;
+                                                 height: 35px;
+                                                 width: 150px;
+                                                 text-align:center;
+                                                 text-indent: -2px;
+                                                 border-radius: 6px;
+                                                 border-width: 3px"),
+                 actionButton("NCA_Delrow","Delete an interval",
+                              style = "color: black;
+                                                 background-color: #CCD1D1;
+                                                 position: relative;
+                                                 left: 3%;
+                                                 height: 35px;
+                                                 width: 150px;
+                                                 text-align:center;
+                                                 text-indent: -2px;
+                                                 border-radius: 6px;
+                                                 border-width: 3px"),br(),
+                 checkboxInput("NCA_ColOption","select NCA parameters",value= F),
+                 checkboxGroupInput("NCA_parameters", label=NULL,choices=c("cmax", "tmax", "cmin",  "tlast", "auclast", "half.life","lambda.z", "aucinf.obs", "aucall","aucinf.pred","tlag", "clast.obs", "vss.obs", "vss.pred"),selected=c("cmax", "tmax", "cmin",  "tlast", "auclast", "half.life","lambda.z", "aucinf.obs","aucall","aucinf.pred"),inline = T),
+                  # uiOutput("NCA_parameters"),
+                 # conditionalPanel("input.Auto_interval==F",uiOutput("NCA_interval")),
+                 actionButton("Run_NCA",label="  RUN NCA",icon =icon("play", lib = "glyphicon"),
+                              style = "color: black;
+                                                 background-color: #4682B4;
+                                                 position: relative;
+                                                 left: 3%;
+                                                 height: 35px;
+                                                 width: 150px;
+                                                 text-align:center;
+                                                 text-indent: -2px;
+                                                 border-radius: 6px;
+                                                 border-width: 3px")
+                 ,br(),
+                 downloadButton("NCA_download", label="Download", class = "butt1",
+                                style = "color: black;
+                                                 background-color: #CCD1D1;
+                                                 position: relative;
+                                                 left: 3%;
+                                                 height: 35px;
+                                                 width: 150px;
+                                                 text-align:center;
+                                                 text-indent: -2px;
+                                                 border-radius: 6px;
+                                                 border-width: 3px")
+                 # sliderInput("PageLength","entries per page",min=10, max=50, value=30, step=10),
+
+
+         ),
+          box(width=8,
+              radioButtons("NCA_Alldata",label="", choices=c("All subjects"=0,"Individuals"=1),selected=0, inline=T),
+               uiOutput("NCA_Subjects"),
+              # select first filter column from fields vector
+                 # shiny::selectInput("filter1", "Select filter column 1:",
+                 #                    choices = NCA_fields),
+              materialSwitch("filter0", "Add a filter ", status = "info", value=F),
+              shiny::uiOutput("filter1"),
+                 # reference a uiOutput that will offer values for first column
+                 shiny::uiOutput("filter1choice"),
+                 # offer a checkbox to allow user to select a second filter
+                 shiny::checkboxInput("filter2req", "Add second filter?"),
+                 # set further conditional panels to appear in the same fashion
+                 shiny::conditionalPanel(condition = 'input.filter2req',
+                                         shiny::uiOutput("filter2eval"),
+                                         shiny::uiOutput("filter2choice"),
+                                         shiny::checkboxInput("filter3req",
+                                                              "Add third filter?")),
+                 shiny::conditionalPanel(condition = 'input.filter3req &
+                                       input.filter2req',
+                                         shiny::uiOutput("filter3eval"),
+                                         shiny::uiOutput("filter3choice"))
+              # ,
+               # column(width=3,uiOutput("Filter_dose"))
+              # column(width=3,uiOutput("Level_dose")),
+              #  uiOutput("NCA_filter1"),
+              # conditionalPanel("input.NCA_filter1 !=='No filter'",uiOutput("filt_Value1")),
+              #  uiOutput("NCA_filter2"),
+              # conditionalPanel("input.NCA_filter2 !=='No filter'",uiOutput("filt_Value2"))
+
+
+              )
+
+
+         ),
+
+         fluidRow(tabBox(
+           title = "Results",
+           width = 12,
+           side = "right",
+           height = "1000px",
+
+           tabPanel("NCA results" , DT::dataTableOutput("NCAdata")),
+           tabPanel ("Data",DT::dataTableOutput("NCA_inputdata"))
+           # tabPanel("Plots")
+           ))
+         #tableOutput("filtered_data_view")
+         )
  )
  )
 ))
